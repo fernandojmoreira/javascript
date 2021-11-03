fec1f170a414ce193b0cbe59deb2bc3e1d925a69
Fernando Moreira <fernandosrmoreira@gmail.com>
2021-11-03 16:07:59 -0300

    Update script.js

--- ex018-exercicio01/ex013 (Modelo)/modelo/script.js
+++ ex018-exercicio01/ex013 (Modelo)/modelo/script.js
@@ -7,19 +7,25 @@ function contar() {
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido. Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
+            // Contagem crescente
             for(let c = i; c <= f; c+= p) {
                 res.innerHTML += `${c} \u{1F449}`
             }
-            res.innerHTML += `\u{1F3C1}`
         } else {
-            for(let c = i; c >= f; c -= p)
-                res.innerHTML +=
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
         }
-            
+        res.innerHTML += `\u{1F3C1}`    
     }
 }
